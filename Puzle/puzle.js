155d23bd550632db5e47660ea4c84aa1e9706f39
Delocraft <ivanbrunoperez@gmail.com>
2025-02-10 14:20:45 +0100

    puzle

--- Puzle/puzle.js
+++ Puzle/puzle.js
@@ -1,8 +1,5 @@
-const DIMENSION_PUZLE = 3;
-const ALTURA_ANCHURA = 500; 
-
-const ALTURA = ALTURA_ANCHURA; 
-const ANCHURA = ALTURA_ANCHURA; 
+const ALTURA_ANCHURA = 500; //no tocar
+const DIMENSION_PUZLE = 5;
 const NUM_PIEZAS = DIMENSION_PUZLE * DIMENSION_PUZLE;
 
 const general = document.getElementById("general");
@@ -15,51 +12,89 @@ boton.className = "boton";
 // imagen
 const imagen = document.createElement("div");
 const imagenActual = "gato.jpg";
-imagen.className = "imagen-puzle";
 imagen.style.backgroundImage = `url(${imagenActual})`;
-imagen.style.width = ANCHURA + "px";
-imagen.style.height = ALTURA + "px";
+imagen.className = "imagen-puzle";
+imagen.style.width = ALTURA_ANCHURA + "px";
+imagen.style.height = ALTURA_ANCHURA + "px";
 
 // puzle
 const puzle = document.createElement("div");
 puzle.className = "puzle";
-puzle.style.width = ALTURA + "px";
-puzle.style.height = ANCHURA + "px";
+puzle.style.width = ALTURA_ANCHURA + "px";
+puzle.style.height = ALTURA_ANCHURA + "px";
 
 let puzleArray = [];
 for(let i = 1; i <= NUM_PIEZAS; i++){
     puzleArray.push(0);
 }
 
-puzleArray.forEach(numero => {
-    let contador = 0;
-    puzleArray.forEach(() => {
-        contador ++;
+function CrearPiezas(){
+    let intervaloX = 0;
+    let contadorX = -1;
+    let contadorY = -1;
+    puzleArray.forEach(numero => {
+        if(intervaloX %DIMENSION_PUZLE === 0){
+            contadorX += 1;
+        }
+        intervaloX += 1;
+        
+        if(contadorY === DIMENSION_PUZLE - 1){
+            contadorY = 0;
+        }else{
+            contadorY += 1;
+        }
+        
+        const cuadricula = document.createElement("div");
+        if (numero === 0) {
+            cuadricula.className = "Vacio";
+        } else if (numero === 1) {
+            cuadricula.className = "Mal";
+        } else if (numero === 2) {
+            cuadricula.className = "Bien";
+        }
+    
+        cuadricula.style.height = Math.sqrt(ALTURA_ANCHURA * ALTURA_ANCHURA / NUM_PIEZAS) + "px";
+        cuadricula.style.width = Math.sqrt(ALTURA_ANCHURA * ALTURA_ANCHURA / NUM_PIEZAS) + "px";
+        cuadricula.ondragover = (e) => e.preventDefault();
+        cuadricula.ondrop = (e) => {
+            e.preventDefault();
+            const id = e.dataTransfer.getData("text");
+            const pieza = document.getElementById(id);
+            if (cuadricula.firstChild) {
+                const reemplazada = cuadricula.firstChild;
+                general.appendChild(reemplazada);
+                reemplazada.style.position = "absolute";
+                reemplazada.style.left = (Math.random() * 31) + 7 + "%";
+                reemplazada.style.top = (Math.random() * 62) + 12 + "%";
+            }
+            cuadricula.appendChild(pieza);
+            pieza.style.position = "relative";
+            pieza.style.left = "0";
+            pieza.style.top = "0";
+        };
+    
+        const pieza = document.createElement("div");
+        pieza.className = "pieza";
+        pieza.id = `pieza-${contadorX}-${contadorY}`;
+        pieza.draggable = true;
+        pieza.ondragstart = (e) => {
+            e.dataTransfer.setData("text", e.target.id);
+        };
+        pieza.style.backgroundImage = `url(${imagenActual})`;
+        pieza.style.height = Math.sqrt(ALTURA_ANCHURA * ALTURA_ANCHURA / NUM_PIEZAS) + "px";
+        pieza.style.width = Math.sqrt(ALTURA_ANCHURA * ALTURA_ANCHURA / NUM_PIEZAS) + "px";
+        
+        let posicionesPiezasX = contadorX*(Math.sqrt(ALTURA_ANCHURA * ALTURA_ANCHURA / NUM_PIEZAS));
+        let posicionesPiezasY = contadorY*(Math.sqrt(ALTURA_ANCHURA * ALTURA_ANCHURA / NUM_PIEZAS));
+    
+        pieza.style.backgroundPosition = `-${posicionesPiezasX}px -${posicionesPiezasY}px`;
+        pieza.style.left = (Math.random() * 31) + 7 + "%";
+        pieza.style.top = (Math.random() * 62) + 12 + "%";
+        general.appendChild(pieza);
+    
+        puzle.appendChild(cuadricula);
     });
-    const cuadricula = document.createElement("div");
-    if (numero === 0) {
-        cuadricula.className = "Vacio";
-    } else if (numero === 1) {
-        cuadricula.className = "Mal";
-    } else if (numero === 2) {
-        cuadricula.className = "Bien";
-    }
-
-    const pieza = document.createElement("div");
-    pieza.className = "pieza";
-    pieza.style.backgroundImage = `url(${imagenActual})`;
-    
-    //-25%
-    pieza.style.backgroundPosition = "-375px -375px";
-    pieza.style.left = (Math.random() * 35) + 7 + "%";
-    pieza.style.top = (Math.random() * 66) + 12 + "%";
-    general.appendChild(pieza);
-    
-    cuadricula.style.height = Math.sqrt(ALTURA * ANCHURA / contador) + "px";
-    cuadricula.style.width = Math.sqrt(ALTURA * ANCHURA / contador) + "px";
-
-    puzle.appendChild(cuadricula);
-});
+}
 
 // añadir
 general.appendChild(imagen);
@@ -67,6 +102,10 @@ general.appendChild(boton);
 general.appendChild(puzle);
 
 boton.addEventListener("click", () => {
-    general.removeChild(imagen);
+    imagen.style.width = ALTURA_ANCHURA / 3 + "px";
+    imagen.style.height = ALTURA_ANCHURA / 3 + "px";
+    imagen.style.left = 55 + "%";
+    imagen.style.border = 3 + "px solid rgb(0, 255, 0)";
+    CrearPiezas();
     puzle.style.display = "flex";
 });
